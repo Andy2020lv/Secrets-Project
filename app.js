7713dd333915437e231f76e4df2ad292c1ec13e8
Andy2020lv <andyhernandez5102@gmail.com>
2022-11-16 05:50:53 -0400

    Completed project with OAuth, google is giving error 400

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const app = express();
 var session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 app.use(express.static("public"));
 app.set("view engine", "ejs")
@@ -31,20 +33,48 @@ mongoose.connect("mongodb://localhost:27017/userDB", {family: 4});
      
 const userSchema = new mongoose.Schema ({
     email: String,
-    password: String
+    password: String,
+    googleId: String,
+    secret: String
+
 });
 
 // The following will hash and salt our passwords, aswell as to save the data in mongodb
 userSchema.plugin(passportLocalMongoose);
+// Plugin findOrCreate
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema)
 
 passport.use(User.createStrategy());
 
 // Serialize creates the cookie
-passport.serializeUser(User.serializeUser());
+passport.serializeUser(function(user, done){
+    done(null, user.id)
+});
 //  Deserialize breaks the cookie and allows the browser to see whats inside of it
-passport.deserializeUser(User.deserializeUser());
+passport.deserializeUser(function(id, done){
+    User.findById(id, function(err, user){
+        done(err, user)
+    })
+});
+
+////////////// GOOGLE OAUTH //////////////////
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackUrl: "http://localhost:3000/auth/google/secrets",
+    redirectUri: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "http://www.googleapis.com/oauth2/v3/userinfo"
+},
+function(accessToken, refreshToken, profile, cb){
+    console.log(profile);
+    User.findOrCreate({googleId: profile.id}, function(err, user){
+        return cb(err, user);
+    });
+    }
+));
 
 
 
@@ -53,6 +83,20 @@ passport.deserializeUser(User.deserializeUser());
 app.get("/", function(req, res){
     res.render("home");
 });
+
+app.get("/auth/google",
+    // Authentificate using google
+    passport.authenticate("google", {scope: ["profile"]})
+);
+
+app.get("/auth/google/secrets",
+passport.authenticate("google", {failureRedirect: "/login"}),
+function(req, res){
+    res.redirect("secrets");
+});
+
+
+
 
 app.get("/login", function(req, res){
     res.render("login");
@@ -63,13 +107,43 @@ app.get("/register", function(req, res){
 });
 
 app.get("/secrets", function(req, res){
+    // The ne means not equal to, so this line of code checks if the secret is not null
+    User.find({"secret": {$ne: null}}, function(err, foundUsers){
+        if (!err){
+            if (foundUsers){
+                res.render("secrets", {userWithSecrets: foundUsers})
+            }
+        }
+    })
+});
+
+app.get("/submit", function(req, res){
     // Check if user is authentificated
-    if (req.isAuthenticated()){
-        res.render("secrets");
-    } else {
+    if(req.isAuthenticated()){
+        res.render("submit");
+    } else{
         res.redirect("/login");
     }
 });
+
+app.post("/submit", function(req, res){
+    const submittedSecret = req.body.secret;
+
+    // This saves the data of the logged in user
+    User.findById(req.user.id, function(err, foundUser){
+        if(!err){
+            if (foundUser){
+                foundUser.secret = submittedSecret,
+                foundUser.save(function(){
+                    res.redirect("/secrets");
+                })
+            }
+        }
+    })
+})
+
+
+
 
 app.get("/logout", function(req, res){
     req.logOut(function(err){
@@ -103,19 +177,15 @@ app.post("/login", function(req, res){
     // The login function comes from passport
     req.login(user, function(err){
         if(!err){
-            passport.authenticate("local");
-            res.redirect("/secrets");
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            });
         } else {
             console.log(err);
         }
     })
    
 });
-
-
-
-
-
 
 
 
